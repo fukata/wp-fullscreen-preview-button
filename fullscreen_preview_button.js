d1c7e1cd6638af8a4d700701f94d8b996c941c83
Tatsuya Fukata <tatsuya.fukata@gmail.com>
2012-08-25 11:27:32 +0900

    when click preview button, click with save button

--- fullscreen_preview_button.js
+++ fullscreen_preview_button.js
@@ -1,25 +1,52 @@
 (function($){
-	$(function(){
-		if ($('#wp-fullscreen-save').size() == 0 || $('#post-preview').size() == 0) {
-			return;
-		}
+    $(function(){
+        if ($('#wp-fullscreen-save').size() == 0 || $('#post-preview').size() == 0) {
+            return;
+        }
 
-		var $preview = $(document.createElement('div'));
-		$preview.attr('id', 'wp-fullscreen-preview');
-		$preview.css({
-			float: 'right',
-			padding: '3px 2px 0 5px'
-		});
-		var $previewButton = $(document.createElement('input'));
-		$previewButton.attr({
-			type: 'button',
-			value: $('#post-preview').text(), 
-		});
-		$previewButton.addClass('button');
-		$previewButton.click(function(){
-			$('#post-preview').click();
-		});
-		$preview.append($previewButton);
-		$preview.insertAfter('#wp-fullscreen-save');
-	});
+        var $preview = $(document.createElement('div'));
+        $preview.attr('id', 'wp-fullscreen-preview');
+        $preview.css({
+            float: 'right',
+            padding: '3px 2px 0 5px'
+        });
+        var $previewButton = $(document.createElement('input'));
+        $previewButton.attr({
+            type: 'button',
+            value: $('#post-preview').text(), 
+        });
+        $previewButton.addClass('button');
+
+        var previewWindow = null;
+        var saveCheckLimit = 10;
+        var saveCheckInterval = 250;
+        $previewButton.click(function() {
+            var saveCheckCount = 0;
+            var clickPreview = function() { $('#post-preview').click(); }
+            var openPreview = function() {
+                saveCheckCount++;
+
+                var isDone = $('#wp-fullscreen-save > img').css('display') === 'none';
+                if ( isDone ) {
+                    clearInterval(timer);
+                    clickPreview();
+                } else {
+                    if ( saveCheckCount >= saveCheckLimit ) {
+                        clearInterval(timer);
+                    }
+                }
+            }
+
+            fullscreen.save();
+            var timer = setInterval(openPreview, saveCheckInterval);
+            if ( previewWindow && !previewWindow.closed ) {
+                previewWindow.close();
+                previewWindow = null;
+            }
+            previewWindow = window.open('about:blank', 'wp-preview');
+            previewWindow.focus();
+        });
+        $preview.append($previewButton);
+        $preview.insertAfter('#wp-fullscreen-save');
+    });
 })(jQuery);
